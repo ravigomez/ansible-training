26effa201c1b9e2fb2655b8f8c306c064034c2b6
Rafael Gómez <ravigomez@gmail.com>
2021-04-11 02:43:57 +0200

    update the key path

--- add-new-host.sh
+++ add-new-host.sh
@@ -4,5 +4,5 @@ psw=`cat password.txt`
 
 ssh -o StrictHostKeyChecking=no -l  $1@$2 "sudo adduser -gecos -p $psw ansible"
 
-ssh-copy-id -i ~./ssh/ansible-id-ed25519.pub ansible@$2
+ssh-copy-id -i ~/.ssh/ansible-id-ed25519.pub ansible@$2
 
