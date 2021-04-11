19bf19f9f6acc677e556b717d4d5d346b8ef19da
Rafael Gómez <ravigomez@gmail.com>
2021-04-11 03:09:08 +0200

    use useradd command instead of adduser

--- add-new-host.sh
+++ add-new-host.sh
@@ -2,7 +2,7 @@
 
 psw=`cat password.txt`
 
-ssh -o StrictHostKeyChecking=no -l  $1@$2 "sudo adduser -gecos -p $psw ansible"
+ssh -o StrictHostKeyChecking=no -l $1@$2 "sudo useradd -m -p $psw -s /bin/bash ansible"
 
 ssh-copy-id -i ~/.ssh/ansible-id-ed25519.pub ansible@$2
 
